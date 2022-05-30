22f8c8989bb78055adf592d8b76876308aa5746a
SelenaGarcia <selenagarcialobo@gmail.com>
2022-05-30 15:46:47 -0300

    Initial Commit

---
+++ calculadoraAutolayoutUITests/calculadoraAutolayoutUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  calculadoraAutolayoutUITestsLaunchTests.swift
+//  calculadoraAutolayoutUITests
+//
+//  Created by Selena Belén Garcia Lobo on 30/05/2022.
+//
+
+import XCTest
+
+class calculadoraAutolayoutUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
